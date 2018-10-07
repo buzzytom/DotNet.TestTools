3a3796f5c8e1a8f082809f5f2db13694c880949b
Thomas Williams <thomas7.williams@hotmail.com>
2018-10-07 18:06:41 +0100

    Implemented a basic configuration system.

---
+++ src/VisualStudio.TestTools.ConsoleApp/Configuration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace VisualStudio.TestTools.ConsoleApp
+{
+    public class Configuration
+    {
+        public Uri[] GetTestProjectUris(Uri workingDirectory)
+        {
+            return TestProjects
+                .Select(x => new Uri(workingDirectory, x))
+                .ToArray();
+        }
+
+        // ----- Properties ----- //
+
+        public string ChangesCommand { set; get; }
+
+        public string[] TestProjects { set; get; }
+    }
+}
