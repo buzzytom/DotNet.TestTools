98b8fe2142d8523010f8f7a9285672c55e7515d4
Thomas Williams <thomas7.williams@hotmail.com>
2018-10-07 13:21:31 +0100

    Added basic code to read projects to the console app.

--- src/VisualStudio.TestTools.ConsoleApp/Program.cs
+++ src/VisualStudio.TestTools.ConsoleApp/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using VisualStudio.TestTools.Projects;
 
 namespace VisualStudio.TestTools.ConsoleApp
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        private static Stopwatch stopwatch = new Stopwatch();
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Uri[] testProjectUris = new Uri[]
+            {
+            };
+
+            // Read the projects structures
+            Console.WriteLine($"Reading {testProjectUris.Length} test project{testProjectUris.Length.GetPluralisation()}...");
+            stopwatch.Start();
+            IEnumerable<Project> projects = ReadProjects(testProjectUris);
+            stopwatch.Stop();
+            Console.WriteLine($"Done! {stopwatch.Elapsed.Milliseconds}ms");
+        }
+
+        private static IEnumerable<Project> ReadProjects(IEnumerable<Uri> paths)
+        {
+            IProjectReader reader = new CsProjectReader();
+            return paths
+                .Select(x => reader.ReadProject(x))
+                .ToArray();
+        }
+
+        private static string GetPluralisation(this int quantity)
+        {
+            if (quantity == 1)
+                return "";
+            else
+                return "s";
         }
     }
 }
