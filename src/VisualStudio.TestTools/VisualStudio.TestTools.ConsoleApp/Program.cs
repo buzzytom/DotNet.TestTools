df17a86c5cbf364c77ebdbd7f3a87d6643e3e03f
Thomas Williams <thomas7.williams@hotmail.com>
2018-10-07 09:30:11 +0100

    Created the solution.

---
+++ src/VisualStudio.TestTools/VisualStudio.TestTools.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualStudio.TestTools.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
